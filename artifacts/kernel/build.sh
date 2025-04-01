a922d3e681c5010a6881c938e9426d4683729bbf
Sergio Medina <lumasepa@gmail.com>
2025-04-01 20:10:33 +0100

    feat: package kernels

---
+++ artifacts/kernel/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+SCRIPT_PATH=$(dirname $(realpath $0))
+
+[[ "$1" != "host" && "$1" != "guest" ]] && echo "Usage: $0 host|guest" && exit 1
+[[  "$2" == "--clean" && -d "$SCRIPT_PATH/build/$1" ]] && sudo rm -rf "$SCRIPT_PATH/build/$1"
+
+[[ ! -d "$SCRIPT_PATH/build/$1" ]] && mkdir -p "$SCRIPT_PATH/build/$1"
+cd "$SCRIPT_PATH/build/$1"
+
+[[ ! -d "AMDSEV" ]] && git clone https://github.com/AMDESE/AMDSEV.git
+cd AMDSEV/
+
+[[ "$(git branch --show-current)" != "snp-latest" ]] && git checkout snp-latest
+COMMIT=$(git rev-parse --short HEAD)
+
+if [[ -n $(git status --porcelain) ]]; then
+  echo "Repo is dirty, not applying the patch AMDSEV.patch, run with --clean start from scratch and apply the patch"
+else
+  git apply ../../../AMDSEV-$1.patch
+fi
+
+docker run -v "$SCRIPT_PATH:/kernel" -it ubuntu:24.04 bash /kernel/docker_build.sh $1
+
+cp $SCRIPT_PATH/build/$1/AMDSEV/linux/*.deb $SCRIPT_PATH/build/$1
+echo "Build finish, artifacts in build/$1"
