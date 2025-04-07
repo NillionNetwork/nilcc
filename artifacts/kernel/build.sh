b4a9f5cad884b9e80196170a62def55d4c058bfa
Sergio Medina <lumasepa@gmail.com>
2025-04-07 14:46:12 +0100

    feat: package ovmf

--- artifacts/kernel/build.sh
+++ artifacts/kernel/build.sh
@@ -24,7 +24,7 @@ else
   git apply ../../../AMDSEV-$1.patch
 fi
 
-docker run -v "$SCRIPT_PATH:/kernel" -it ubuntu:24.04 bash /kernel/docker_build.sh $1
+docker run --rm -v "$SCRIPT_PATH:/kernel" -it ubuntu:24.04 bash /kernel/docker_build.sh $1
 
 cp $SCRIPT_PATH/build/$1/AMDSEV/linux/*.deb $SCRIPT_PATH/build/$1
 echo "Build finish, artifacts in build/$1"
--- artifacts/qemu/docker_build.sh
+++ artifacts/qemu/docker_build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # This is the second phase of the process of building a static qemu with AMDSEV support.
 # This script is called inside of docker by the build.sh script, go there to understand the whole context.
+set -e
 
 apk update
-apk add gcc autoconf automake libtool git sqlite ninja vim bash git python3 musl-dev build-base glib-dev gettext-dev shadow sudo meson iasl \
+apk add gcc autoconf automake libtool sqlite ninja vim bash git python3 musl-dev build-base glib-dev gettext-dev shadow sudo meson iasl \
  glib-static gettext-static zlib-static util-linux-static bzip2-static ncurses-static libslirp-dev zlib-dev zlib-static libxkbcommon-static \
  libxkbcommon-dev libx11-static zstd-static zstd-dev zstd-libs libpng libpng-dev libpng-static libselinux libselinux-dev libselinux-static \
- libudev-zero libudev-zero-dev pixman pixman-dev pixman-static
+ libudev-zero libudev-zero-dev pixman pixman-dev pixman-static nasm perl
 
 # install static libslirp
 git clone https://gitlab.freedesktop.org/slirp/libslirp.git /tmp/libslirp
