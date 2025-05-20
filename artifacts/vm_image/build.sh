16b36e63073604a6ec7f4e1481a6758a3165ca28
Sergio Medina <lumasepa@gmail.com>
2025-05-20 16:06:47 +0100

    feat: extract kernel and initrd in vm install

--- artifacts/vm_image/build.sh
+++ artifacts/vm_image/build.sh
@@ -18,7 +18,10 @@ QEMU_STATIC_PATH=${QEMU_STATIC_CHECK[0]}
 
 [[ $1 == "--clean" && -d "$SCRIPT_PATH/build" ]] && sudo rm -rf "$SCRIPT_PATH/build"
 
+KERNEL_PATH="$SCRIPT_PATH/build/kernel/$TYPE"
+
 [[ ! -d "$SCRIPT_PATH/build/vm_images" ]] && mkdir -p "$SCRIPT_PATH/build/vm_images"
+[[ ! -d "$KERNEL_PATH" ]] && mkdir -p "$KERNEL_PATH"
 [[ ! -d "$SCRIPT_PATH/build/qemu" ]] && mkdir -p "$SCRIPT_PATH/build/qemu"
 
 # Install static qemu
@@ -34,17 +37,20 @@ SSH_FORWARD_PORT=2221
 
 # Install ubuntu on VM
 sudo $QEMU_PATH/qemu-system-x86_64 \
-  -enable-kvm -nographic -no-reboot -cpu EPYC-v4 -machine q35 \
-  -smp 12,maxcpus=31 -m 16G,slots=5,maxmem=120G \
-  -drive if=pflash,format=raw,unit=0,file=$SCRIPT_PATH/build/qemu/usr/local/share/qemu/OVMF.fd,readonly=on \
+ -enable-kvm -nographic -no-reboot -cpu EPYC-v4 -machine q35 \
+ -smp 12,maxcpus=31 -m 16G,slots=5,maxmem=120G \
+ -drive if=pflash,format=raw,unit=0,file=$SCRIPT_PATH/build/qemu/usr/local/share/qemu/OVMF_CODE.fd,readonly=on \
  -drive file=$VM_IMAGE_PATH,if=none,id=disk0,format=qcow2 \
  -device virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true \
  -device scsi-hd,drive=disk0 \
  -device virtio-net-pci,disable-legacy=on,iommu_platform=true,netdev=vmnic,romfile= \
  -netdev user,id=vmnic,hostfwd=tcp::$SSH_FORWARD_PORT-:22 \
- -cdrom $AUTOINSTALL_UBUNTU_ISO_PATH
+ -cdrom $AUTOINSTALL_UBUNTU_ISO_PATH \
+ -virtfs local,path="$KERNEL_PATH",mount_tag=hostshare,security_model=passthrough,id=hostshare
 
 sudo chown -R $(whoami) $SCRIPT_PATH/build/
 
 [[ ! -d $SCRIPT_PATH/../dist/vm_images ]] && mkdir -p $SCRIPT_PATH/../dist/vm_images
 cp $VM_IMAGE_PATH "$SCRIPT_PATH/../dist/vm_images/"
+[[ ! -d "$SCRIPT_PATH/../dist/vm_images/kernel/" ]] && mkdir -p "$SCRIPT_PATH/../dist/vm_images/kernel/"
+cp -r $KERNEL_PATH "$SCRIPT_PATH/../dist/vm_images/kernel/"
