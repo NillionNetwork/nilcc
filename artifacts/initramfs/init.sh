fb7607064668434d8b7d1f7435c8e561a2b22f45
Matias Fontanini <matias.fontanini@gmail.com>
2025-05-27 10:06:13 -0700

    feat: add disk integrity checks on vm start

--- artifacts/initramfs/init.sh
+++ artifacts/initramfs/init.sh
@@ -19,19 +19,61 @@ mkdir /dev/pts
 mount -t devpts -o noexec,nosuid,gid=5,mode=0620 devpts /dev/pts || true
 
 MNT_DIR=/root
-ROOT=/dev/sda2
-BOOT_PROOF_DIR=$MNT_DIR/etc/nilcc-boot
+BOOT_PROOF_DIR=$MNT_DIR/var/lib/nilcc-boot
 
-mount $ROOT $MNT_DIR
+# Parse command line options
+for x in $(cat /proc/cmdline); do
+  case $x in
+  root=*)
+    ROOT=${x#root=}
+    ;;
+  verity_disk=*)
+    VERITY_DISK=${x#verity_disk=}
+    ;;
+  verity_roothash=*)
+    VERITY_ROOT_HASH=${x#verity_roothash=}
+    ;;
+  state_disk=*)
+    STATE_DISK=${x#state_disk=}
+    ;;
+  esac
+done
 
-# Note, eventually we will want to not do this rm so `mkdir` fails. For now it's good to have it so we can
-# boot a vm multiple times from the same .cow2
-rm -rf $BOOT_PROOF_DIR
-mkdir $BOOT_PROOF_DIR
+# unlock verity device
+veritysetup open $ROOT root $VERITY_DISK $VERITY_ROOT_HASH
 
-head -c 64 /dev/urandom >$BOOT_PROOF_DIR/input
+# mount root disk as read-only
+mount -o ro,noload /dev/mapper/root $MNT_DIR
 
+# Generate a random password and use LUKS to encrypt the state disk with it.
+STATE_PASSWORD=$(head -c 64 /dev/random | base64 -w 0)
+echo "$STATE_PASSWORD" | cryptsetup luksFormat "$STATE_DISK"
+
+# Now open the disk and format it using ext4
+echo "$STATE_PASSWORD" | cryptsetup luksOpen "$STATE_DISK" state
+mkfs.ext4 /dev/mapper/state
+unset STATE_PASSWORD
+
+# Mount the now encrypted and formatted state disk on /var and clear it up.
+mount /dev/mapper/state "${MNT_DIR}/var"
+
+# Now copy over the original /var into the new one.
+cp -r "${MNT_DIR}/ro/var" "${MNT_DIR}/"
+
+# Create a tmpfs for /etc and copy over the /ro contents to it
+mount -t tmpfs -o size=1024M tmpfs "$MNT_DIR/etc"
+cp -r "${MNT_DIR}/ro/etc" "${MNT_DIR}/"
+
+# Create a tmpfs for /tmp.
+mount -t tmpfs -o size=1024M tmpfs "$MNT_DIR/tmp"
+
+# Create a mount where the cvm-agent will mount the docker-compose ISO.
+mount -t tmpfs -o size=4M tmpfs "$MNT_DIR/media/cvm-agent-entrypoint"
+
+# Generate an attestation using random data
 modprobe sev-guest
+mkdir -p $BOOT_PROOF_DIR
+head -c 64 /dev/urandom >$BOOT_PROOF_DIR/input
 /opt/nillion/initrd-helper report $BOOT_PROOF_DIR/report.json --data $(cat $BOOT_PROOF_DIR/input | base64 -w 0)
 
 mount --move /proc $MNT_DIR/proc
--- artifacts/vm_image/launch_vm.sh
+++ artifacts/vm_image/launch_vm.sh
@@ -10,10 +10,25 @@ SCRIPT_PATH=$(dirname $(realpath $0))
 VM_IMAGE="${1:-$SCRIPT_PATH/build/vm_images/ubuntu24.04-cpu.qcow2}"
 [[ ! -f "$VM_IMAGE" ]] && echo "VM image not found, run 'build.sh' first" && exit 1
 
+VM_IMAGE_HASH_DEV="${1:-$SCRIPT_PATH/build/vm_images/ubuntu24.04-cpu-verity/verity-hash-dev}"
+[[ ! -f "$VM_IMAGE_HASH_DEV" ]] && echo "VM disk hashes dev not found, run 'build.sh' first" && exit 1
+
+VM_IMAGE_ROOT_HASH=$(cat "${1:-$SCRIPT_PATH/build/vm_images/ubuntu24.04-cpu-verity/root-hash}")
+
 INITRD="${INITRD:-$SCRIPT_PATH/../initramfs/build/initramfs.cpio.gz}"
 [[ ! -f "$INITRD" ]] && echo "initrd not found, run '../initramfs/build.sh' first" && exit 1
 
+STATE_DISK=$(mktemp)
+
+cleanup() {
+  rm ${STATE_DISK}
+}
+
+trap cleanup EXIT SIGINT
+
 QEMU_BASE_PATH="$SCRIPT_PATH/build/qemu/"
+$QEMU_BASE_PATH/usr/local/bin/qemu-img create -f raw "$STATE_DISK" 10G
+
 $QEMU_BASE_PATH/usr/local/bin/qemu-system-x86_64 \
   -enable-kvm -nographic -no-reboot \
   -machine confidential-guest-support=sev0,vmport=off \
@@ -22,9 +37,15 @@ $QEMU_BASE_PATH/usr/local/bin/qemu-system-x86_64 \
   -bios $QEMU_BASE_PATH/usr/local/share/qemu/OVMF.fd \
   -kernel ${SCRIPT_PATH}/build/kernel/cpu/vmlinuz-6.11.0-snp-guest-98f7e32f20d2 \
   -initrd "${INITRD}" \
-  -append "console=ttyS0 earlyprintk=serial" \
+  -append "console=ttyS0 earlyprintk=serial root=/dev/sda2 verity_disk=/dev/sdb verity_roothash=${VM_IMAGE_ROOT_HASH} state_disk=/dev/sdc" \
   -drive file=$VM_IMAGE,if=none,id=disk0,format=qcow2 \
   -device virtio-scsi-pci,id=scsi0,disable-legacy=on,iommu_platform=true \
   -device scsi-hd,drive=disk0,bootindex=1 \
-  -drive file=/tmp/nilcc.iso,id=disk1,media=cdrom,readonly=true \
+  -drive file=$VM_IMAGE_HASH_DEV,if=none,id=disk1,format=raw \
+  -device virtio-scsi-pci,id=scsi1,disable-legacy=on,iommu_platform=true \
+  -device scsi-hd,drive=disk1,bootindex=2 \
+  -drive file=$STATE_DISK,if=none,id=disk2,format=raw \
+  -device virtio-scsi-pci,id=scsi2,disable-legacy=on,iommu_platform=true \
+  -device scsi-hd,drive=disk2,bootindex=3 \
+  -drive file=/tmp/nilcc.iso,id=disk3,media=cdrom,readonly=true \
   -fw_cfg name=opt/ovmf/X-PciMmio64Mb,string=151072
