62a8fce7a2f245fd6e26842ab41675c4155cc9d6
José Cabrero-Holgueras <jose.cabrero@nillion.com>
2025-07-15 17:35:18 +0200

    fix: fix alpine build version for Qemu and add pcre2-static dependency

--- artifacts/qemu/docker_build.sh
+++ artifacts/qemu/docker_build.sh
@@ -7,7 +7,7 @@ apk update
 apk add gcc autoconf automake libtool sqlite ninja vim bash git python3 musl-dev build-base glib-dev gettext-dev shadow sudo meson iasl \
  glib-static gettext-static zlib-static util-linux-static bzip2-static ncurses-static libslirp-dev zlib-dev zlib-static libxkbcommon-static \
  libxkbcommon-dev libx11-static zstd-static zstd-dev zstd-libs libpng libpng-dev libpng-static libselinux libselinux-dev libselinux-static \
- libudev-zero libudev-zero-dev pixman pixman-dev pixman-static nasm perl
+ libudev-zero libudev-zero-dev pixman pixman-dev pixman-static nasm perl pcre2-static
 
 # install static libslirp
 git clone https://gitlab.freedesktop.org/slirp/libslirp.git /tmp/libslirp
