8456383bcbc45b4476c91dec7a8c2879911555b3
Matias Fontanini <matias.fontanini@gmail.com>
2025-07-31 09:54:05 -0700

    chore: enable CORS in openapi endpoint

--- nilcc-api/src/openapi/openapi.router.ts
+++ nilcc-api/src/openapi/openapi.router.ts
@@ -1,12 +1,14 @@
 import { openAPISpecs } from "hono-openapi";
 import { PathsV1 } from "#/common/paths";
 import type { ControllerOptions } from "#/common/types";
+import { cors } from 'hono/cors'
 
 export function createOpenApiRouter(options: ControllerOptions): void {
   const { app } = options;
 
   app.get(
     PathsV1.docs,
+    cors(),
     openAPISpecs(app, {
       documentation: {
         info: {
