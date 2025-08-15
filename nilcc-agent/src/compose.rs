88ef06fa59c8211587d287c0252c3babcbfc8444
Matias Fontanini <matias.fontanini@gmail.com>
2025-08-15 10:59:31 -0700

    feat: don't allow mounting volumes outside of `FILES`

--- nilcc-agent/src/compose.rs
+++ nilcc-agent/src/compose.rs
@@ -1,6 +1,8 @@
 use cvm_agent_models::bootstrap::{CADDY_ACME_EAB_KEY_ID, CADDY_ACME_EAB_MAC_KEY};
-use docker_compose_types::{Compose, Ports, PublishedPort};
-use std::iter;
+use docker_compose_types::{
+    Compose, ComposeVolume, EnvFile, MapOrEmpty, Ports, PublishedPort, TopLevelVolumes, Volumes,
+};
+use std::{collections::HashSet, iter};
 
 const RESERVED_CONTAINERS: &[&str] = &["nilcc-attester", "nilcc-proxy"];
 const RESERVED_PORTS: &[u16] = &[80, 443];
@@ -20,6 +22,7 @@ pub(crate) fn validate_docker_compose(
     if compose.services.is_empty() {
         return Err(Error::Invalid("no services defined".into()));
     }
+    let top_level_volumes = validate_top_level_volumes(&compose.volumes)?;
     let mut found_public_container = false;
     for (name, service) in &compose.services.0 {
         let service = service.as_ref().ok_or_else(|| Error::Invalid(format!("no body in service '{name}'")))?;
@@ -34,7 +37,17 @@ pub(crate) fn validate_docker_compose(
             if name == public_container_name {
                 found_public_container = true;
             }
-            validate_ports(&service.ports)?;
+        }
+
+        validate_ports(&service.ports)?;
+        if service.privileged {
+            return Err(Error::PrivilegedService);
+        }
+        for volume in &service.volumes {
+            validate_volumes(volume, &top_level_volumes)?;
+        }
+        if let Some(env) = &service.env_file {
+            validate_env_file(env)?;
         }
     }
     if found_public_container {
@@ -42,6 +55,21 @@ pub(crate) fn validate_docker_compose(
     } else {
         Err(Error::PublicContainer(public_container_name.to_string()))
     }
+}
+
+fn validate_top_level_volumes(volumes: &TopLevelVolumes) -> Result<HashSet<&str>, DockerComposeValidationError> {
+    let mut output = HashSet::new();
+    for (name, volume) in &volumes.0 {
+        if let MapOrEmpty::Map(volume) = volume {
+            let ComposeVolume { driver, driver_opts, external, labels, name } = volume;
+            if driver.is_some() || !driver_opts.is_empty() || external.is_some() || !labels.is_empty() || name.is_some()
+            {
+                return Err(DockerComposeValidationError::VolumeAttributes);
+            }
+        }
+        output.insert(name.as_str());
+    }
+    Ok(output)
 }
 
 fn validate_ports(ports: &Ports) -> Result<(), DockerComposeValidationError> {
@@ -115,9 +143,46 @@ impl PublishedPortExt for PublishedPort {
     }
 }
 
+fn validate_volumes(volume: &Volumes, top_level_volumes: &HashSet<&str>) -> Result<(), DockerComposeValidationError> {
+    use DockerComposeValidationError as Error;
+    let Volumes::Simple(spec) = volume else {
+        return Err(Error::LongFormVolumes);
+    };
+
+    let Some((source, _)) = spec.split_once(':') else { return Err(Error::VolumeColon) };
+    if top_level_volumes.contains(source) {
+        return Ok(());
+    }
+    validate_volume_path(source)
+}
+
+fn validate_volume_path(path: &str) -> Result<(), DockerComposeValidationError> {
+    use DockerComposeValidationError as Error;
+    if !path.starts_with("$FILES") && !path.starts_with("${FILES}") {
+        return Err(Error::FilesEnvVar);
+    }
+    if path.contains("../") {
+        Err(Error::MountDotDot)
+    } else {
+        Ok(())
+    }
+}
+
+fn validate_env_file(env: &EnvFile) -> Result<(), DockerComposeValidationError> {
+    match env {
+        EnvFile::Simple(path) => validate_volume_path(path),
+        EnvFile::List(paths) => {
+            for path in paths {
+                validate_volume_path(path)?;
+            }
+            Ok(())
+        }
+    }
+}
+
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum DockerComposeValidationError {
-    #[error("maformed docker compose: {0}")]
+    #[error("malformed docker compose: {0}")]
     Malformed(#[from] serde_yaml::Error),
 
     #[error("invalid docker compose: {0}")]
@@ -137,13 +202,30 @@ pub(crate) enum DockerComposeValidationError {
 
     #[error("container {0} is not part of compose file")]
     PublicContainer(String),
+
+    #[error("volumes can only use short form")]
+    LongFormVolumes,
+
+    #[error("missing ':' in volume mount")]
+    VolumeColon,
+
+    #[error("mounts need to start from the $FILES environment variable")]
+    FilesEnvVar,
+
+    #[error("mounts cannot use '../'")]
+    MountDotDot,
+
+    #[error("volume definitions cannot contain any attributes")]
+    VolumeAttributes,
+
+    #[error("privileged services are not allowed")]
+    PrivilegedService,
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
-
     use super::*;
+    use rstest::rstest;
 
     fn validate_success(compose: &str, public_container_name: &str) {
         validate_docker_compose(compose, public_container_name).expect("validation failed");
@@ -191,8 +273,18 @@ services:
       - "86-87:80-81/tcp"
       - "86-87:80-81/udp"
     environment:
-      FOO: \${ FOO_VAR }
+      FOO: ${FOO_VAR}
+    volumes:
+      - "$FILES/foo:/tmp/foo"
+      - $FILES/foo:/tmp/bar
+      - ${FILES}/foo:/tmp/tar
+      - other:/tmp/other
+    env_file:
+      - $FILES/dotenv1
+      - ${FILES}/dotenv2
     command: "caddy"
+volumes:
+  other:
 "#;
         validate_success(compose, "api");
     }
@@ -226,6 +318,18 @@ services:
     command: "caddy"
 "#;
         validate_failure(compose, "other", DockerComposeValidationError::PublicContainer("other".into()));
+    }
+
+    #[test]
+    fn privileged_service() {
+        let compose = r#"
+services:
+  api:
+    image: caddy:2
+    command: "caddy"
+    privileged: true
+"#;
+        validate_failure(compose, "api", DockerComposeValidationError::PrivilegedService);
     }
 
     #[rstest]
@@ -307,5 +411,37 @@ services:
 "#
         );
         validate_failure(&compose, "api", DockerComposeValidationError::ReservedEnv(env));
+    }
+
+    #[rstest]
+    #[case::no_colom("/tmp/hello", DockerComposeValidationError::VolumeColon)]
+    #[case::no_files("/tmp/hello:", DockerComposeValidationError::FilesEnvVar)]
+    #[case::dotdot("$FILES/../proc/foo:", DockerComposeValidationError::MountDotDot)]
+    fn invalid_volume_source(#[case] source: &'static str, #[case] error: DockerComposeValidationError) {
+        let compose = format!(
+            r#"
+services:
+  api:
+    image: caddy:2
+    volumes:
+      - {source}/tmp/foo
+"#
+        );
+        validate_failure(&compose, "api", error);
+    }
+
+    #[rstest]
+    #[case::no_files("/tmp/hello", DockerComposeValidationError::FilesEnvVar)]
+    #[case::dotdot("$FILES/../proc/foo", DockerComposeValidationError::MountDotDot)]
+    fn invalid_env_file(#[case] env_file: &'static str, #[case] error: DockerComposeValidationError) {
+        let compose = format!(
+            r#"
+services:
+  api:
+    image: caddy:2
+    env_file: {env_file}
+"#
+        );
+        validate_failure(&compose, "api", error);
     }
 }
