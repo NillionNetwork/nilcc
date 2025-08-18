4ca7bda9fa14ccd29dcdbd9f61fb512b40cf6f87
Matias Fontanini <matias.fontanini@gmail.com>
2025-08-18 11:18:47 -0700

    fix: disable logging for all loggers in nvtrust

--- nilcc-attester/gpu-attester/main.py
+++ nilcc-attester/gpu-attester/main.py
@@ -9,17 +9,19 @@ import logging
 NRAS_URL = "https://nras.attestation.nvidia.com/v3/attest/gpu"
 
 
-def disable_logging():
-    logger = logging.getLogger("sdk-logger")
+def disable_logging(name: str):
+    logger = logging.getLogger(name)
     logger.setLevel(logging.CRITICAL + 1)
-    handler = logging.StreamHandler(sys.stdout)
+    handler = logging.StreamHandler(sys.stderr)
     logger.addHandler(handler)
 
 
 def main():
     # Disable logging before we import the nvidia package since it otherwise logs
-    disable_logging()
+    disable_logging("sdk-logger")
     from nv_attestation_sdk import attestation
+
+    disable_logging("gpu-verifier-info")
 
     parser = argparse.ArgumentParser(prog="gpu-attester")
     parser.add_argument("nonce", type=str)
@@ -38,7 +40,7 @@ def main():
         sys.exit(1)
 
     token = client.get_token()
-    b64_token = base64.b64encode(token.encode("utf-8")).decode("utf-8")
+    b64_token = base64.b64encode(token.encode("utf-8")).decode("utf-8").strip()
     print(b64_token)
 
 
