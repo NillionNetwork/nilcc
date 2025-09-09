ff72096a42221da4b19fb07206c7c1386fb24035
Matias Fontanini <matias.fontanini@gmail.com>
2025-09-09 12:21:02 -0700

    feat: add endpoint in nilcc-attester that exposes build version

---
+++ nilcc-attester/build.rs
@@ -0,0 +1,22 @@
+use std::{
+    process::Command,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+fn run_command<const N: usize>(command: &str, args: [&str; N]) -> String {
+    let output = Command::new(command).args(args).output().expect("failed to get run command");
+    String::from_utf8(output.stdout).expect("invalid command output")
+}
+
+fn git_hash() -> String {
+    run_command("git", ["rev-parse", "--short", "HEAD"])
+}
+
+fn main() {
+    let hash = git_hash();
+    let unix_now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+    println!("cargo:rustc-env=BUILD_GIT_COMMIT_HASH={hash}");
+    println!("cargo:rustc-env=BUILD_TIMESTAMP={unix_now}");
+    println!("cargo:rustc-rerun-if-changed=.git/HEAD");
+}
--- nilcc-attester/src/routes/mod.rs
+++ nilcc-attester/src/routes/mod.rs
@@ -2,14 +2,16 @@ use crate::{config::VmType, report::HardwareReporter};
 use axum::{routing::get, Router};
 use std::sync::Arc;
 
+pub(crate) mod about;
 pub(crate) mod health;
 pub(crate) mod report;
 
 /// Build a router for the entire service.
 pub fn build_router(state: AppState) -> Router {
-    Router::new()
-        .route("/health", get(health::handler))
-        .nest("/api/v1", Router::new().route("/report", get(report::handler).with_state(state)))
+    Router::new().route("/health", get(health::handler)).nest(
+        "/api/v1",
+        Router::new().route("/about", get(about::handler)).route("/report", get(report::handler).with_state(state)),
+    )
 }
 
 #[derive(Clone)]
