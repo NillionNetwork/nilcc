02f2bdec2faf2121fdc8e1ac527fc82ab538e4a2
Matias Fontanini <matias.fontanini@gmail.com>
2025-09-17 13:57:22 -0700

    feat: shrink VM image by using ubuntu cloud minimal + squashfs

--- artifacts/generate-metadata.sh
+++ artifacts/generate-metadata.sh
@@ -13,13 +13,13 @@ INITRD=initramfs/initramfs.cpio.gz
 INITRD_HASH=$(sha256sum "$SCRIPT_PATH/dist/$INITRD" | cut -d " " -f 1)
 OVMF=vm_images/ovmf/OVMF.fd
 OVMF_HASH=$(sha256sum "$SCRIPT_PATH/dist/$OVMF" | cut -d " " -f 1)
-CPU_DISK=vm_images/cvm-cpu.qcow2
+CPU_DISK=vm_images/cvm-cpu.squashfs
 CPU_DISK_HASH=$(sha256sum "$SCRIPT_PATH/dist/$CPU_DISK" | cut -d " " -f 1)
 CPU_VERITY_ROOT_HASH=$(cat "$SCRIPT_PATH/dist/vm_images/cvm-cpu-verity/root-hash")
 CPU_VERITY_HASHES_DISK=vm_images/cvm-cpu-verity/verity-hash-dev
 CPU_KERNEL=vm_images/kernel/cpu-vmlinuz
 CPU_KERNEL_HASH=$(sha256sum "$SCRIPT_PATH/dist/$CPU_KERNEL" | cut -d " " -f 1)
-GPU_DISK=vm_images/cvm-gpu.qcow2
+GPU_DISK=vm_images/cvm-gpu.squashfs
 GPU_DISK_HASH=$(sha256sum "$SCRIPT_PATH/dist/$GPU_DISK" | cut -d " " -f 1)
 GPU_VERITY_ROOT_HASH=$(cat "$SCRIPT_PATH/dist/vm_images/cvm-gpu-verity/root-hash")
 GPU_VERITY_HASHES_DISK=vm_images/cvm-gpu-verity/verity-hash-dev
@@ -46,12 +46,12 @@ METADATA=$(
     "sha256": "${INITRD_HASH}"
   },
   "cvm": {
-    "cmdline": "panic=-1 root=/dev/sda2 verity_disk=/dev/sdb verity_roothash={VERITY_ROOT_HASH} state_disk=/dev/sdc docker_compose_disk=/dev/sr0 docker_compose_hash={DOCKER_COMPOSE_HASH}",
+    "cmdline": "panic=-1 root=/dev/sda verity_disk=/dev/sdb verity_roothash={VERITY_ROOT_HASH} state_disk=/dev/sdc docker_compose_disk=/dev/sr0 docker_compose_hash={DOCKER_COMPOSE_HASH}",
     "images": {
       "cpu": {
         "disk": {
           "path": "${CPU_DISK}",
-          "format": "qcow2",
+          "format": "raw",
           "sha256": "${CPU_DISK_HASH}"
         },
         "verity": {
@@ -69,7 +69,7 @@ METADATA=$(
       "gpu": {
         "disk": {
           "path": "${GPU_DISK}",
-          "format": "qcow2",
+          "format": "raw",
           "sha256": "${GPU_DISK_HASH}"
         },
         "verity": {
