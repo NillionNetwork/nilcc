2e15285ee976bb574786766eef4862ac819b6c1a
Matias Fontanini <matias.fontanini@gmail.com>
2026-01-15 11:17:41 -0800

    feat: add initial heartbeat funder

---
+++ heartbeat-funder/src/main.rs
@@ -0,0 +1,66 @@
+use crate::{
+    config::Config,
+    monitor::{Monitor, MonitorArgs},
+};
+use anyhow::bail;
+use clap::Parser;
+use tokio::signal::{self, unix::SignalKind};
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::filter::EnvFilter;
+
+mod config;
+mod monitor;
+
+/// Heartbeat funder allows monitoring and funding wallets that are used to emit heartbeats in
+/// nilcc workloads.
+#[derive(Parser)]
+struct Cli {
+    /// The path to the config file.
+    #[clap(short, long)]
+    config_path: Option<String>,
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c().await.expect("failed to install ctrl-c handler");
+    };
+
+    let terminate = async {
+        signal::unix::signal(SignalKind::terminate()).expect("failed to install signal handler").recv().await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {
+            info!("Received ctrl-c");
+        },
+        _ = terminate => {
+            info!("Received SIGTERM");
+        },
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let _ = rustls::crypto::ring::default_provider().install_default();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::builder().with_default_directive(LevelFilter::INFO.into()).from_env_lossy())
+        .init();
+
+    let cli = Cli::parse();
+    let config = Config::load(cli.config_path.as_deref())?;
+    if config.thresholds.eth.minimum >= config.thresholds.eth.target {
+        bail!("ETH minimum funding threshold must be lower than its target");
+    }
+
+    Monitor::spawn(MonitorArgs {
+        rpc_endpoint: config.rpc.endpoint,
+        signer: config.private_key,
+        static_addresses: config.wallets,
+        poll_interval: config.interval_seconds,
+        thresholds: config.thresholds,
+    });
+
+    shutdown_signal().await;
+    Ok(())
+}
